433d3c83f441b25dfe2e3d34c372fe0c2128b7fe
aliiyitutuncu <mehmet.ali.iyitutuncu@gmail.com>
2022-01-01 04:57:22 +0300

    days 40 projects update. Flight Club Project

--- days-040/data_manger.py
+++ days-040/data_manger.py
@@ -1,7 +1,9 @@
 import requests
+import os
 
-SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/cf15e951620d40c82cdd92658ac08e2d/flightDeals/prices"
-TOKEN = "UserSecretTokenOfSheety"
+SHEETY_PRICES_ENDPOINT = os.environ.get("SHEETY_PRICES_ENDPOINT")
+SHEETY_USERS_ENDPOINT = os.environ.get("SHEETY_USERS_ENDPOINT")
+TOKEN = os.environ.get("TOKEN")
 
 class DataManager:
 
@@ -26,3 +28,10 @@ class DataManager:
                 json=new_data
             )
             print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = SHEETY_USERS_ENDPOINT
+        response = requests.get(url=customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
