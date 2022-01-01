cb0afe33865e371104b0cde981dced299ee245cb
aliiyitutuncu <mehmet.ali.iyitutuncu@gmail.com>
2022-01-01 21:35:32 +0300

    Days 40 projects update. Flight Club Project

--- days-040/main.py
+++ days-040/main.py
@@ -44,12 +44,15 @@ for destination_code in destinations:
         emails = [row["email"] for row in users]
         names = [row["firstName"] for row in users]
 
-        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to " \
+                  f"{flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
 
         if flight.stop_overs > 0:
             message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
 
-        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+        link = f"https://www.google.co.uk/flights?hl=en#flt=" \
+               f"{flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}." \
+               f"{flight.origin_airport}.{flight.return_date}"
 
         notification_manager.send_emails(emails, message, link)
 
