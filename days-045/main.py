c8fe8322f31292e433a469e6e46b483216507b19
aliiyitutuncu <mehmet.ali.iyitutuncu@gmail.com>
2022-01-12 15:45:49 +0300

    introduction to BeautifulSoup

--- days-045/main.py
+++ days-045/main.py
@@ -1,8 +1,83 @@
 from bs4 import BeautifulSoup
-# import lxml
+import requests
 
-with open("website.html") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/show")
+yc_web_page = response.text
 
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.title.name)
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="a", class_="titlelink")
+article_text = []
+article_link = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_text.append(text)
+    link = article_tag.get("href")
+    article_link.append(link)
+
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+# print(largest_index)
+
+
+print(article_text[largest_index])
+print(article_link[largest_index])
+print(article_upvotes[largest_index])
+
+# print(int(article_upvotes[0]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import lxml
+#
+# # with open("website.html") as file:
+# #     contents = file.read()
+#
+# file = codecs.open("website.html", "r", "utf-8")
+# contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# # print(soup.title)
+# # print(soup.title.string)
+#
+# # print(soup.prettify())
+#
+# all_anchor_tags = soup.find_all(name="a")
+# # print(all_anchor_tags)
+# #
+# # for tag in all_anchor_tags:
+# #     # print(tag.getText())
+# #     print(tag.get("href"))
+#
+# #
+# # heading = soup.find(name="h1", id="name")
+# # print(heading)
+# #
+# # section_heading = soup.find(name="h3", class_="heading")
+# # print(section_heading.getText())
+#
+# company_url = soup.select_one(selector="#name")
+# # print(company_url)
+#
+# heading = soup.select(".heading")
+# print(heading)
+#
+#
